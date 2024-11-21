7aad0483143ee626ceedd074cf8df97c20579e7f
Jonny West <jonnyappleidnew@gmaill.com>
2024-11-21 12:20:37 +0000

    docker file framework, plus pipline testing

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# this is where the actual ingestion code will go
+
+print(f'job finished successfully for day = {day}')
